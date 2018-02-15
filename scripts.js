1643660367a81858f23159dfd4f1f665e9579e25
Jonathan Fejtek <jonathan.fejtek@mail.utoronto.ca>
2018-02-15 12:08:32 -0500

    changed a line

--- scripts.js
+++ scripts.js
@@ -18,8 +18,6 @@ agesApp.userSelection = function () {
    });
 }
 
-
-
 //function that takes the input of the slider and puts that into the '_century' value of the data request.
 agesApp.setupSlider = function(){
       $('.century-input').on("change",function(){
